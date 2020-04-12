23bf976b284e7c1466ab95524201bf5d6cc8a39f
Sagie <Sagie012@gmail.com>
2020-04-12 20:58:35 +0300

    Add logs and pollInterval

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ const config: Config = Environment.getConfig();
 // your implementing service names and URLs
 const gateway = new ApolloGateway({
   serviceList: config.microServices,
+  debug: true,
+  experimental_pollInterval: 60000
 });
 
 // Pass the ApolloGateway to the ApolloServer constructor
